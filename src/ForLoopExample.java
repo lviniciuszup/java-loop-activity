eefbd1906d00beefea7258d5b19c9663d8415c22
lviniciuszup <luiz.vincius@zup.com.br>
2024-12-06 11:53:54 -0300

    Add exercise print array using a for loop

---
+++ src/ForLoopExample.java
@@ -0,0 +1,8 @@
+public class ForLoopExample{
+    public static void main(String[] args){
+        int[] numeros = {1,2,3,4,5};
+        for(int i = 0 ; i < numeros.length ; i++){
+            System.out.println("Numero é: " +numeros[i]);
+        }
+    }
+}
